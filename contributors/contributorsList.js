2a1e2e27b0ba44464d3ccc2e5ad6a59a86bbaed3
Pallavipankaj <itsmepallavi28@gmail.com>
2023-10-19 23:26:59 +0530

    opensource

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1,1 +1,5 @@
+"name"- "Pallavipankaj",
+"tech-stack"-"C, C++, JavaScript",
+"Quote"-"Cocooned in the illusion of mastery, I believed I had learned enough. Yet, amidst the brilliance of others, I discovered I still lingered at the precipice."
+
 
